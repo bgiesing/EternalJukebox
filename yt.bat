3ed0a1abc356e5d33d77826e9f3c9b92f8f49ad6
floriegl <22279483+floriegl@users.noreply.github.com>
2026-03-02 21:59:41 +0100

    Add ytl-dlp ejs

--- yt.bat
+++ yt.bat
@@ -2,4 +2,4 @@ IF "%~4"=="" GOTO download
 SET %2="%1=%2"
 SHIFT
 :download
-yt-dlp --audio-format %3 -x -o %2 --max-filesize 100m --no-playlist --max-downloads 1 --playlist-end 1 --exec "echo Video ID: %%(id)s" -- "%1"
+yt-dlp --js-runtimes node --remote-components ejs:github --audio-format %3 -x -o %2 --max-filesize 100m --no-playlist --max-downloads 1 --playlist-end 1 --exec "echo Video ID: %%(id)s" -- "%1"
